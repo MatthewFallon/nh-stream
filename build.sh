8131c928409d2e425b3f8fa9c4d74e5cc793a7fd
Matthew Fallon <matthew.b.fallon@gmail.com>
2024-12-24 21:03:32 -0600

    Splitting up script and adding array for file ordering.

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-pandoc ./index.md ./jobs.md ./power-on.md ./computer.md ./cheatsheet.md --from markdown -o nhstream.pdf --template ./eisvogel.latex
+shopt -s extglob
+
+# for each in !(README|LICENSE).md; do
+#     printf "Converting %s\n" $each
+#     pandoc $each --from markdown -o "nhstream-$(basename $each .md).pdf" --template ./eisvogel.latex
+# done
+
+# qpdf --empty --pages $(printf "nhstream-%s.pdf " "${arr[@]}") -- nhstream.pdf
+
+arr=(index power-on jobs computer cheatsheet)
+
+pandoc $(printf "%s.md " "${arr[@]}") --from markdown -o "nhstream.pdf" --template ./eisvogel.latex
+
+shopt -u extglob
 
 bundle exec jekyll build
