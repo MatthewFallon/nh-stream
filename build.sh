cc126e5d1830d5ff4230bc47f31844dea2925050
Matthew Fallon <matthew.b.fallon@gmail.com>
2025-03-23 12:02:25 -0500

    Finished new build procedure with pdftk

--- build.sh
+++ build.sh
@@ -2,16 +2,20 @@
 
 shopt -s extglob
 
-# for each in !(README|LICENSE).md; do
-#     printf "Converting %s\n" $each
-#     pandoc $each --from markdown -o "nhstream-$(basename $each .md).pdf" --template ./eisvogel.latex
-# done
+arr=(index power-on jobs computer cheatsheet)
+
+for each in "${arr[@]}"; do
+    printf "Converting %s\n" $each
+    pandoc "$each.md" --from markdown -o "nhstream-$each.pdf" --template ./eisvogel.latex
+done
+
+pdftk $(printf "nhstream-%s.pdf " "${arr[@]}") cat output nhstream.pdf
 
 # qpdf --empty --pages $(printf "nhstream-%s.pdf " "${arr[@]}") -- nhstream.pdf
 
-arr=(index power-on jobs computer cheatsheet)
+# arr=(index power-on jobs computer cheatsheet)
 
-pandoc $(printf "%s.md " "${arr[@]}") --from markdown -o "nhstream.pdf" --template ./eisvogel.latex
+# pandoc $(printf "%s.md " "${arr[@]}") --from markdown -o "nhstream.pdf" --template ./eisvogel.latex
 
 shopt -u extglob
 
